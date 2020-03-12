ec34fbe4352ed75398ebb158d3a161c2927f7f17
Stephane Comeau <scomea@microsoft.com>
2020-03-12 12:38:28 -0700

    working

--- packages/fast-components/src/anchored-region/anchored-region.template.ts
+++ packages/fast-components/src/anchored-region/anchored-region.template.ts
@@ -1,8 +1,25 @@
-import { html } from "@microsoft/fast-element";
+import { html, ref } from "@microsoft/fast-element";
 import { AnchoredRegion } from "./anchored-region";
+import { unset } from "lodash-es";
 
 export const AnchoredRegionTemplate = html<AnchoredRegion>`
-  <div class="root">
+  <template>
+      <div
+          class="region"
+          ${ref("region")}
+          style="
+              position:relative; 
+              left: ${x => x.regionLeft}; 
+              right: ${x => x.regionRight}; 
+              top: ${x => x.regionTop}; 
+              bottom: ${x => x.regionBottom}; 
+          "
+    >
     <slot></slot>
-  </div>
+    </div>
+    ${x => x.regionLeft}
+    ${x => x.regionRight}
+    ${x => x.regionTop}
+    ${x => x.regionBottom}
+  </template>
 `;
